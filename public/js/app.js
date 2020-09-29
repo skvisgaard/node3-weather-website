d5e24c54ed5c87e0356214ba6e0c4cdfbadd9099
Sofie <skvisgaard@gmail.com>
2020-09-29 10:48:47 +0200

    Remove unnesserary conlose log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side js is loaded');
-
 // fetch('http://localhost:3000/weather?address=boston').then((response) => {
 //     response.json().then((data) => {
 //         if (data.error) {
