efcb7e7b8df20301715a1946642de42a086dbd7e
Sofie <skvisgaard@gmail.com>
2020-09-29 13:50:55 +0200

    Added uv-index to weather data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress. There is an UV index of ' + body.current.uv_index + '.');
         }
     })
 }
